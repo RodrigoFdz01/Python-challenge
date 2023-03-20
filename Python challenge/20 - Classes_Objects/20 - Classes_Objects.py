8d0804822a77889ac3e94f372e78b0cac97414bc
RodrigoFdz01 <rodrigof3012@gmail.com>
2023-03-19 23:32:04 -0300

    new module

---
+++ Python challenge/20 - Classes_Objects/20 - Classes_Objects.py
@@ -0,0 +1,25 @@
+class Person:
+      def __init__ (self, name):
+        # self allows to attach parameter to the class
+          self.name =name
+
+p = Person('Asabeneh')
+print(p.name)
+print(p)
+
+
+class Person:
+      def __init__(self, firstname, lastname, age, country, city):
+          self.firstname = firstname
+          self.lastname = lastname
+          self.age = age
+          self.country = country
+          self.city = city
+
+
+p = Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+print(p.firstname)
+print(p.lastname)
+print(p.age)
+print(p.country)
+print(p.city)
